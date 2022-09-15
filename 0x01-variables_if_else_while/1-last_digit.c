a280333cbb302c2a144d01dff9f1f8d235b6605c
emmyxclusive <emmyxclusive@gmail.com>
2022-09-15 04:12:37 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,8 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
-*
 * main - print if the number is postive, zero, or negative
 *
 * Description: using the main function
@@ -13,7 +10,7 @@
 * Return: 0
 */
 int main(void)
-/*code goes here */
+   /*code goes here */
 {
 int n;
 int l;
