a0739471a26ee9b25ffa1dcb5d66477662fa780b
emmyxclusive <emmyxclusive@gmail.com>
2022-09-17 22:27:27 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 /**
  *print_last_digit - function that computes the absolute value of an integer
  *@c:  is the int that will use for the argument of the function
+ *
  *Return: 0
  */
 int print_last_digit(int c)
