7b412129c05a385f5b921fbafed39993c5c74d40
emmyxclusive <emmyxclusive@gmail.com>
2022-09-18 05:00:44 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,7 +31,7 @@ _putchar((result / 10) % 10 + '0');
 }
 else if (result <= 99 && result >= 10)
 {
-_putchar((result / 10) + '0')
+_putchar((result / 10) + '0');
 }
 _putchar((result % 10) + '0');
 }
