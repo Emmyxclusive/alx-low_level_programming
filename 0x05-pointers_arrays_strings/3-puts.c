5f5df3b1015e80d9aed26c05cee24474ae344e9d
emmyxclusive <emmyxclusive@gmail.com>
2022-09-19 21:30:16 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: the string
+ *
+ * Description: print a string
+ * on succes: return no error
+ */
+void _puts(char *s)
+int i = 0;
+while (*(str + i) != ('\0')
+{
+putchar(*(str + i));
+i++;
+}
+putchar(10);
+}
